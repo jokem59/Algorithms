30c76d33ba184d433a2c5cd02db6d69e6eaee77c
Joe Kim <joe.kim509@gmail.com>
2019-03-14 00:29:08 -0700

    combo lock done

--- CompetitiveProgramming/Chapter1/Exercise1_2_3.cpp
+++ CompetitiveProgramming/Chapter1/Exercise1_2_3.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 
-// UVa 00272 TEX Quotes
+// UVa 00272 TEX Quotes using std::cin
 void replaceQuotes()
 {
     bool first = true;
@@ -30,6 +30,30 @@ void replaceQuotes()
     return;
 }
 
+// UVa 00272 TEX Quotes using scanf
+void replaceQuotes2()
+{
+    bool first = true;
+    char curChar;
+
+    while (scanf("%c", &curChar) != EOF)
+    {
+        if (curChar == '"') {
+            if (first) {
+                printf("``");
+                first = false;
+            }
+            else {
+                printf("''");
+                first = true;
+            }
+        }
+        else
+            printf("%c", curChar);
+    }
+    return;
+}
+
 // UVa 01124 Celebrity Jeopardy
 void celebJeopardy()
 {
@@ -43,10 +67,28 @@ void celebJeopardy()
     }
 }
 
+// UVa 10550 Combination Lock; first number in input is starting position of dial
+void combinationLock()
+{
+    int start, first, second, third;
+
+    scanf("%d %d %d %d", &start, &first, &second, &third);
+
+    // as long as one value is NOT zero
+    while (start || first || second || third) {
+        // multiple by 9 to get the value in degrees; 360 / 40 = 9;
+        printf("%d\n",
+               1080 + ((start - first + 40) % 40 + (second - first + 40) % 40 + (second - third + 40) % 40) * 9);
+
+        scanf("%d %d %d %d", &start, &first, &second, &third);
+    }
+
+    return;
+}
+
 int main() {
     std::ios_base::sync_with_stdio(false);  // std::cin now faster than scanf
 
-    celebJeopardy();
-
+    combinationLock();
     return 0;
 }
