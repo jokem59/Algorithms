59b68bf7e9214664cdfdd00c740b35c19960ebe5
Joe Kim <joe.kim509@gmail.com>
2019-03-16 21:52:00 -0700

    more problems solved

--- CompetitiveProgramming/Chapter1/Exercise1_2_3.cpp
+++ CompetitiveProgramming/Chapter1/Exercise1_2_3.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 // UVa 00272 TEX Quotes using std::cin
 void replaceQuotes()
@@ -123,12 +125,149 @@ void relationalOperators() {
 
 // UVa 11498 Division of Nlogonia
 void divisionNlogonia() {
+    int numCases, n, m;
+    scanf("%d", &numCases);
 
+    while (numCases != 0) {
+        scanf("%d %d", &n, &m);
+
+        while (numCases > 0) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+
+            if (x == n || y == m)
+                printf("%s\n", "divisa");
+            if (x < n && y > m)
+                printf("%s\n", "NO");
+            if (x < n && y < m)
+                printf("%s\n", "SO");
+            if (x > n && y > m)
+                printf("%s\n", "NE");
+            if (x > n && y < m)
+                printf("%s\n", "SE");
+            numCases--;
+        }
+
+        scanf("%d", &numCases);
+    }
+
+    return;
+}
+
+// UVa 11727 Cost Cutting
+void costCutting() {
+    int numCases, x, y, z;
+    scanf("%d", &numCases);
+
+    for (int i = 0; i < numCases; i++) {
+        std::vector<int> input;
+        scanf("%d %d %d", &x, &y, &z);
+        input.push_back(x);
+        input.push_back(y);
+        input.push_back(z);
+        std::sort(input.begin(), input.end());
+
+        printf("Case %d: %d\n", i + 1, input[1]);
+    }
+
+    return;
+}
+
+// UVa 10114 Loansome Car Buyer
+void carBuyer() {
+    int monthNums, numDeprecations, currentDecrease, currentMonth;
+    double carTotal, downPayment, loanOwed, amountPayedPerMonth;
+
+    std::vector<int> decreaseTime;
+    std::vector<double> decrease;
+
+    scanf("%d", &monthNums);
+
+    while (monthNums > 0)
+    {
+        scanf("%lf %lf %d", &downPayment, &loanOwed, &numDeprecations);
+
+        amountPayedPerMonth = loanOwed / monthNums;
+
+        carTotal = downPayment + loanOwed;
+
+        decreaseTime.resize(numDeprecations + 1);
+        decrease.resize(numDeprecations + 1);
+
+        for (int i = 0; i < numDeprecations; ++i)
+            scanf("%d %lf", &decreaseTime[i], &decrease[i]);
+
+        decreaseTime[numDeprecations] = 101;
+
+        currentDecrease = 0;
+
+        carTotal -= carTotal * decrease[currentDecrease];
+
+        for (currentMonth = 1; carTotal < loanOwed; ++currentMonth)
+        {
+            loanOwed -= amountPayedPerMonth;
+
+            if (decreaseTime[currentDecrease + 1] == currentMonth)
+                ++currentDecrease;
+
+            carTotal -= carTotal * decrease[currentDecrease];
+        }
+
+        --currentMonth;
+
+        if (currentMonth == 1)
+            printf("%d month\n", currentMonth);
+        else
+            printf("%d months\n", currentMonth);
+
+        scanf("%d", &monthNums);
+    }
+}
+
+// UVa 11559 Event Planning
+void eventPlanning() {
+    // # participants, budget, num hotels, number of weeks
+
+    // multiple test cases
+    int numPeople, budget, numHotels, numWeeks;
+    while(scanf("%d %d %d %d", &numPeople, &budget, &numHotels, &numWeeks) == 4) {
+        // printf("numPeople: %d | budget: %d | numHotels: %d | numWeeks: %d\n", numPeople, budget, numHotels, numWeeks);
+        bool stayHome = false;
+        int cost = budget + 1;
+
+        for(; numHotels > 0; numHotels--) {
+            int dailyRate, numBeds;
+            scanf("%d", &dailyRate);
+            // printf("Daily rate for current hotel: %d\n", dailyRate);
+
+            for(int i = numWeeks; i > 0; i--) {
+                scanf("%d", &numBeds);
+                // printf("numBeds: %d\n", numBeds);
+                if (numBeds < numPeople)
+                    continue;
+
+                int curTripCost = (dailyRate * numPeople);
+                // printf("curTripCost: %d\n", curTripCost);
+                cost = curTripCost < cost ? curTripCost : cost;
+            }
+        }
+        if (cost > budget)
+            printf("stay home\n");
+        else
+            printf("%d\n", cost);
+    }
+    return;
+}
+
+// UVa 11799 Horror Dash
+void horrorDash() {
+
+    return;
 }
 
 int main() {
     std::ios_base::sync_with_stdio(false);  // std::cin now faster than scanf
 
-    relationalOperators();
+    horrorDash();
     return 0;
 }
