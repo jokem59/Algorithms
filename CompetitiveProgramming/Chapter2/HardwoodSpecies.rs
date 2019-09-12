17f9792d360b600e0ef287f755e8783b971f8b32
Joseph Kim <joskim@microsoft.com>
2019-09-12 14:33:14 -0700

    some logic error, stuck in forever loop for hardwoodspecies.rs

--- CompetitiveProgramming/Chapter2/HardwoodSpecies.rs
+++ CompetitiveProgramming/Chapter2/HardwoodSpecies.rs
@@ -24,7 +24,7 @@ use std::collections::BTreeMap;
 // fn main() {
 //     let mut input = String::new();
 //     io::stdin().read_line(&mut input);
-//     let numCases: i32 = input.parse::<i32>().unwrap();
+//     let numCases: i32 = input.parse::<i32>();
 
 //     for _i in 0..numCases {
 //         let mut treeMap = BTreeMap::new();
@@ -53,18 +53,28 @@ use std::collections::BTreeMap;
 
 
 // Iteration 1: Below implementaiton fixes the primary issue and conforms to snake case styling.
-// the BTreeMap is using <&String, i32>, therefore, the tree_map is holding an immutable reference to input.  Then we try and get
+// Provide credit to: https://users.rust-lang.org/t/cannot-borrow-string-as-mutable-because-it-is-also-borrowed-as-immutable/32476
+//
+// The BTreeMap is using <&String, i32>, therefore, the tree_map is holding an immutable reference to input.  Then we try and get
 // a mutable reference to input and Rust disallows this.  If this was allowed, we would be changing the key in the tree_map which breaks
 // the invariant provided by BTreeMap: https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
 // "It is a logic error for a key to be modified in such a way that the key's ordering relative to any other key, as determined by the
 // Ord trait, changes while it is in the map."
 //
+// Explain how clone() achieves this.
+//
 // Make comments on unwrap().
+//
+// Make comments on what read_line() is actually parsing (newline chars?)
+//
+// Comment on String::len() vs String::chars().count()
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let num_cases: i32 = input.parse::<i32>().unwrap();
+    println!("Num test cases: {} | Char count: {}", input, input.chars().count());
+    let num_cases: u32 = input.trim().parse::<u32>().unwrap();
+    println!("Integer num cases: {}", num_cases);
 
     for _i in 0..num_cases {
         let mut tree_map = BTreeMap::new();
@@ -72,6 +82,7 @@ fn main() {
 
         loop {
             io::stdin().read_line(&mut input).unwrap();
+            println!("{}", input);
             if input.len() == 0 { break; }
 
             if !tree_map.contains_key(&input) {
@@ -90,3 +101,31 @@ fn main() {
         }
     }
 }
+
+
+// Iteration 2: Syntatic sugar changes
+// Comment on Entry()
+// Comment on .and_modify()
+// Comment on .or_insert()
+// Comment on the style
+
+// fn main() {
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input).unwrap();
+//     let num_cases: i32 = input.parse::<i32>().unwrap();
+//     for _i in 0..num_cases {
+//         let mut tree_map : BTreeMap<String, i32> = BTreeMap::new();
+//         let mut total: f32 = 0.0;
+//         loop {
+//             io::stdin().read_line(&mut input).unwrap();
+//             if input.len() == 0 { break; }
+//             tree_map.entry(input.clone())
+//                 .and_modify(|count| { *count += 1 })
+//                 .or_insert(1);
+//             total += 1.0;
+//         }
+//         for (key, val) in &tree_map {
+//             println!("{} {}", key, (*val as f32) / total * 100.0);
+//         }
+//     }
+// }
