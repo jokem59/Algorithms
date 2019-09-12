791c02927f192cb5f08ea19544fd02a3d4d95788
Joe Kim <joe.kim509@gmail.com>
2019-09-12 00:58:09 -0700

    stuck on rust E0502

--- CompetitiveProgramming/Chapter2/HardwoodSpecies.rs
+++ CompetitiveProgramming/Chapter2/HardwoodSpecies.rs
@@ -6,18 +6,32 @@ use std::collections::BTreeMap;
 
 // Next step is to read input as integer
 
-fn main()
-{
+fn main() {
     let mut input = String::new();
-    match io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read from stdin");
-    {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("{}", input);
-        }
-        Err(error) => println!("error: {}", error),
-    }
+    io::stdin().read_line(&mut input);
+    let numCases: i32 = input.parse::<i32>().unwrap();
 
+    for _i in 0..numCases {
+        let mut treeMap = BTreeMap::new();
+        let mut total: f32 = 0.0;
+
+        loop {
+            io::stdin().read_line(&mut input);
+            if input.len() == 0 { break; }
+
+            if !treeMap.contains_key(&input) {
+                treeMap.insert(&input, 1);
+            }
+            else {
+                if let Some(count) = treeMap.get_mut(&input) {
+                    *count += 1;
+                }
+            }
+            total += 1.0;
+        }
+
+        for (key, val) in &treeMap {
+            println!("{} {}", key, (*val as f32) / total * 100.0);
+        }
+    }
 }
